cf103724d3b32b668410f4c5b0160eca1a569b93
worm333 <flexblademaster@gmail.com>
2017-01-30 11:35:43 +0200

    - fixed naming issues

--- src/main/java/org/gluu/message/consumer/config/MySQLConfiguration.java
+++ src/main/java/org/gluu/message/consumer/config/MySQLConfiguration.java
@@ -1,7 +1,7 @@
 package org.gluu.message.consumer.config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
-import org.gluu.message.consumer.config.condition.PostgreSQLCondition;
+import org.gluu.message.consumer.config.condition.MySQLCondition;
 import org.gluu.message.consumer.config.util.Constants;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
@@ -15,18 +15,18 @@ import org.springframework.context.annotation.Profile;
  * Created by eugeniuparvan on 1/27/17.
  */
 @Profile(Constants.SPRING_PROFILE_PRODUCTION)
-@Conditional(PostgreSQLCondition.class)
+@Conditional(MySQLCondition.class)
 @Configuration
-public class PostgreSQLConfiguration_ {
+public class MySQLConfiguration {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.psql.jpa")
+    @ConfigurationProperties(prefix = "spring.mysql.jpa")
     public JpaProperties jpaProperties() {
         return new JpaProperties();
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.psql.datasource")
+    @ConfigurationProperties(prefix = "spring.mysql.datasource")
     public DataSource dataSource() {
         return (DataSource) DataSourceBuilder.create().type(DataSource.class).build();
     }
